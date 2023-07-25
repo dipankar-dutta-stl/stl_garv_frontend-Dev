254f8c24fa452a526becf1383ce1588f64c79b57
dipankar-dutta-stl <dipankar.dutta@stl.tech>
2023-07-25 12:20:21 +0530

    25-07-2023 12:20

--- libs/environment/src/lib/environment.interface.ts
+++ libs/environment/src/lib/environment.interface.ts
@@ -1,11 +1,7 @@
 export interface Environment {
     production: boolean;
     apiURL: string;
-<<<<<<< HEAD
     apiURL_Ecomm:string;
     apiURLEntertain:String;
-=======
-    apiURLEntertain: string;
->>>>>>> origin/entertainment
     cloudFrontURL: string;
-}
+} 
